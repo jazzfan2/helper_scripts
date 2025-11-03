e3e909f5a3ac4d1b13b703cf94153b1bf16d30eb
jazzfan2 <149713903+jazzfan2@users.noreply.github.com>
2025-11-03 19:32:52 +0100

    First commit

--- splitpanes.sh
+++ splitpanes.sh
@@ -13,8 +13,8 @@
 # as an argument. It finds the active file manager window by its process- and window-ID,
 # and keeps track of all windows that originated from it by subsequent splitting, even
 # if replaced by use of the program 'changedir.sh'. It does so by maintaining a
-# so-called 'relations-file' in RAM, by which it supervises all started sequences of
-# window splitting.
+# so-called 'relations-file' in RAM, by which it supervises all sequences of
+# window splitting started after login.
 #
 # Meant to be launched from the tools-menu of the file-manager named 'XFile' (part of
 # 'Enhanced Motif Window Manager' by Alexander Pampuchin

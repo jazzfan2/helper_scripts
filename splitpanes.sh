faa9f1d0d10d034973c9e3920a091e6965a7b2f4
jazzfan2 <149713903+jazzfan2@users.noreply.github.com>
2025-11-05 23:35:13 +0100

    First commit

--- splitpanes.sh
+++ splitpanes.sh
@@ -128,7 +128,7 @@ if [[ $mode == "unite" ]]; then
     geom=${relatedpanes[0]}
 
     # Start new file-manager window with current directory and original (unsplit) geometry:
-    xfile -a -l -geometry "$geom" "$directory" &
+    xfile -a -l -geometry "$geom" "$directory"
 
     # Kill all process-IDs found in the same line, all being the related split windows:
     for i in ${!relatedpanes[@]}; do

425e724a87afbdcbed83182195816b82928922de
jazzfan2 <149713903+jazzfan2@users.noreply.github.com>
2025-11-06 21:35:03 +0100

    First commit

--- selfmounter.sh
+++ selfmounter.sh
@@ -10,7 +10,7 @@
 # file (part of the 'Enhanced Motif Window Manager (EMWM)' by
 # Alexander Pampuchin - https://fastestcode.org/ - LGPLv3, MIT License).
 #
-# Alternative for using a udev-rule for automounting.
+# Alternative for udevil() or using a udev-rule for automounting.
 #
 ######################################################################################
 #
